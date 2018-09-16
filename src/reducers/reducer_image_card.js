382d99e100453cfd34ffe269295e2652f91c4b05
asankagit <inbox2asanka@gmail.com>
2018-09-16 12:18:17 +0530

    add more imgae card to default view

--- src/reducers/reducer_image_card.js
+++ src/reducers/reducer_image_card.js
@@ -4,7 +4,12 @@ export default function (state = {}, action) {
         case 'RESPONSE_REC':
             return [
                 { title: 'new aba' ,url:'https://grantha.lk/media/catalog/product/cache/bfa4f59b59693a18ca4208a6e02422cd/n/a/narichchi_saha_nawwi.jpg',description:''},
-                { title: 'new response',url:'https://grantha.lk/media/catalog/product/cache/bfa4f59b59693a18ca4208a6e02422cd/n/a/narichchi_saha_nawwi.jpg',description:'' },
+                { title: 'new response',url:'https://grantha.lk/media/catalog/product/cache/bfa4f59b59693a18ca4208a6e02422cd/d/o/dosthara_haramitta.jpg',description:'' },
+                { title: 'new request',url:'https://grantha.lk/media/catalog/product/cache/bfa4f59b59693a18ca4208a6e02422cd/b/o/bonchi_ata_mole.jpg',description:'' },
+				{ title: 'new aba' ,url:'https://grantha.lk/media/catalog/product/cache/bfa4f59b59693a18ca4208a6e02422cd/n/a/narichchi_saha_nawwi.jpg',description:''},
+                { title: 'new response',url:'https://grantha.lk/media/catalog/product/cache/bfa4f59b59693a18ca4208a6e02422cd/d/o/dosthara_haramitta.jpg',description:'' },
+                { title: 'new request',url:'https://grantha.lk/media/catalog/product/cache/bfa4f59b59693a18ca4208a6e02422cd/n/a/narichchi_saha_nawwi.jpg',description:'' },{ title: 'new response',url:'https://grantha.lk/media/catalog/product/cache/bfa4f59b59693a18ca4208a6e02422cd/d/o/dosthara_haramitta.jpg',description:'' },
+                { title: 'new request',url:'https://grantha.lk/media/catalog/product/cache/bfa4f59b59693a18ca4208a6e02422cd/n/a/narichchi_saha_nawwi.jpg',description:'' },{ title: 'new response',url:'https://grantha.lk/media/catalog/product/cache/bfa4f59b59693a18ca4208a6e02422cd/d/i/dik_dandaa_saha_mahabada.jpg',description:'' },
                 { title: 'new request',url:'https://grantha.lk/media/catalog/product/cache/bfa4f59b59693a18ca4208a6e02422cd/n/a/narichchi_saha_nawwi.jpg',description:'' }
             ];
         case 'DEFAULT_IMAGE_CARD_REC':
